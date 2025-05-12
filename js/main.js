c7bdf1f5889fbb511c0e5618298c2ae1f40e3397
jaschwanewede <jaschwanewede@davidson.edu>
2025-05-12 19:47:30 -0400

    initial tasks working!

--- js/main.js
+++ js/main.js
@@ -1,117 +1,101 @@
+let globalGames; //global variable, so can be accessed from the onchange in html
+
 /**
  * Load data from JSON file asynchronously and render force directed graph
  */
 d3.json('data/ssbu_with_neighbor_counts.json').then(data => {
-    const forceDirectedGraph = new ForceDirectedGraph({ parentElement: '#force-directed-graph'}, data);
+  globalGames = data;  
+  
+  const forceDirectedGraph = new ForceDirectedGraph({ parentElement: '#force-directed-graph'}, globalGames);
   
   
   //working on dropdown filtering
 
   //code taken from Lab 2
 
-  dataFiltering(data);
+  dataFiltering();
   
   })
   .catch(error => console.error(error));
 
 
 
-function dataFiltering(data) {
-	var games = data;
-
-	/* **************************************************
-	 *
-	 * ADD YOUR CODE HERE (ARRAY/DATA MANIPULATION)
-	 *
-	 * CALL THE FOLLOWING FUNCTION TO RENDER THE BAR-CHART:
-	 *
-	 * renderBarChart(data)
-	 *
-	 * - 'data' must be an array of JSON objects
-	 * - the max. length of 'data' is 5
-	 *
-	 * **************************************************/
-
-
-	
-  
+function dataFiltering() {
+	var games = globalGames.nodes.slice(); //copies 
+	  
 	var currentArray = dataManipulation(); //returns current selected box value
 
   var currentAttr = currentArray[0];
   var currentSort = currentArray[1];
 
 
-  if (currentSort === "ascend") {
-    games.sort( function(a, b){
-		
-    if(currentAttr === "copies")
-		  return games.size.ascending; 
-  	
-    else if (currentAttr === "crossover") {
-      return games.neighborCount.ascending;
+  sortedGames = games.sort(function(a, b) {
 
-    }
+        if (currentAttr === "copies") {
+            
+          return currentSort === "ascend" ? a.size - b.size : b.size - a.size;
+        
+          } else if (currentAttr === "crossover") {
+            
+            return currentSort === "ascend" ? a.neighborCount - b.neighborCount : b.neighborCount - a.neighborCount;
+  
+          } else if (currentAttr === "alphabetical") {
 
-    else if (currentAttr === "alphabetical") {
-
-      return games.id.ascending;
-    }
+            //used Perplexity to help with id comparison, alphabetical
+            if (currentSort === "ascend") {
+                  return a.id.localeCompare(b.id);
+              } else {
+                  return b.id.localeCompare(a.id);
+              }
+          }
   });
 
-  } else if (currentSort === "descend") {
-    
-    if(currentAttr === "copies")
-		  return games.size.descending; 
-  	
-    else if (currentAttr === "crossover") {
-      return games.neighborCount.descending;
-
-    }
-
-    else if (currentAttr === "alphabetical") {
-
-      return games.id.descending;
-    }
-
+    updateRank(sortedGames, currentAttr, currentSort);
+   
   }
-  //we want to sort
-  
-
-  //not filtering, but can use framework
-	/** 
-  var top5;
-	if (currentSelected != "all") {
-		top5 = attractions.filter( function(attraction, i) {
-			console.log("Filtered by category");
-			return (attraction.Category == currentSelected);
-			 }); 
-
-		top5 = top5.filter( function(index, i) {
-				console.log("Filtered to top5");
-				return (i < 5);
-				 }); 
-	
-	} else {
-		
-		top5 = attractions.filter( function(index, i) {
-		console.log("Filtered");
-		return (i < 5);
-		 });
-	
-	}
-	renderBarChart(top5);
-	console.log("rendered");
-  */
-}
 
 function dataManipulation() {
 	var attribute = document.getElementById("attr");
 	var selectedAttr = attribute.options[attribute.selectedIndex].value;
 
   var sorting = document.getElementById("sort");
-  var selectedSort = sorting.options[attribute.selectedIndex].value;
+  var selectedSort = sorting.options[sorting.selectedIndex].value;
 
 	console.log(selectedAttr, selectedSort)
 
 	return [selectedAttr, selectedSort];
+}
+
+function updateRank(games, currentAttr, currentSort) {
+  const dropdown = d3.select("#order");
+  dropdown.selectAll('option').remove();
+  
+  if (currentAttr === "copies") {
+    games.forEach(game => {
+            dropdown.append('option')
+              .attr('value', game.id)
+              .text(`${game.id}: ${game.size}M copies sold`);
+            });
+  }
+
+  else if (currentAttr === "crossover") {
+    games.forEach(game => {
+            dropdown.append('option')
+              .attr('value', game.id)
+              .text(`${game.id}: ${game.neighborCount} Crossovers`);
+            });
+
+  }
+
+  else if (currentAttr === "alphabetical") {
+    games.forEach(game => {
+            dropdown.append('option')
+              .attr('value', game.id)
+              .text(`${game.id}`);
+            });
+
+
+          }
+
+   
 }
